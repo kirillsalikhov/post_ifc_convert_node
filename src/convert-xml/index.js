c1f1614683a33a93e803bc49b6af87969fda44ca
kirill <kirill.salikhov@gmail.com>
2024-01-22 18:02:32 +0400

    feat add write objects.json
    refs: IV-CS-SUPPORT-T-61

--- src/convert-xml/index.js
+++ src/convert-xml/index.js
@@ -1,11 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const StreamPromises = require("stream/promises");
+
+const { JsonStreamStringify } = require('json-stream-stringify');
+
 const {Parser} = require("./Parser");
 
 async function convertXml(inputXml, output) {
     const parser = new Parser(inputXml);
     await parser.parse();
+
+    const result = [];
     for(let el of parser.iterateNodeTree()) {
-        console.log(el.tagName);
+        result.push(el.tagName);
     }
+
+    await StreamPromises.pipeline(
+        new JsonStreamStringify(result),
+        fs.createWriteStream(path.join(output, 'objects.json'))
+    );
 }
 
 exports.convertXml = convertXml;
