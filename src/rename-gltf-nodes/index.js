bd0f370244cf86dcf8a56bb9918e91118d9eee73
Mikhail Turkeev <turkeevm@gmail.com>
2024-01-27 00:18:53 +0400

    improve comments

--- src/rename-gltf-nodes/index.js
+++ src/rename-gltf-nodes/index.js
@@ -14,17 +14,14 @@ const gltfNameToIfcId = (gltfName) => {
 
     // name pattern: product-b55ee828-24bc-45a5-b012-c4b3130dbdab-body
     // bytes in section:     4        2    2    2    6
-    // string offset:        8        17   22   27   32    38
-    // firstByte + words:    bb000000-1111-1122-2222-333333444444
-    //                       b - firstByte, 0..4 -> words[0..4]
+    // string offset:        8 10     17  22 24 27   32    38
+    //                       v v      v    v v  v    v     v
+    // uint24s:              00111111-2222-2233-3333-444444555555
+    //                       0..5 -> uint24s[0..5]
 
     // the rest is just converting 32-digit base16 number weirdly spread across the string to 22-digit base64 number.
     // (both may include leading zeroes)
     // first we convert it to 1 uint8 + 5 uint24 stored in uint32[6]
-    // then just convert them to base64
-    //                                                vvvv - these are also zeroes. So the first guid char can only be 0, 1, 2 or 3
-    // uint8 number in binary:  xxxxxxxx_xxxxxxxx_xxxx0000_00111111 x - are zeroes (we ignore them), 000..., 111... etc. - bits of corresponding digit in base64
-    // uint24 number in binary: xxxxxxxx_00000011_11112222_22333333
     uint24s[0] = parseInt(gltfName.slice(8, 10), 16);// (4 leading hex-zeroes) + 2 hex digits
     uint24s[1] = parseInt(gltfName.slice(10, 16), 16);// 6 hex digits
     uint24s[2] = parseInt(gltfName.slice(17, 21) + gltfName.slice(22, 24), 16);// 4 + 2 hex digits
@@ -33,6 +30,10 @@ const gltfNameToIfcId = (gltfName) => {
     uint24s[5] = parseInt(gltfName.slice(38, 44), 16);// 6 hex digits
     //todo: .slice and general-case parseInt probably slows it down. Might get better perf if parsed manually.
 
+    // then convert them to base64 one by one.
+    // uint8 number in binary:  xxxxxxxx_xxxxxxxx_xxxx0000_00111111 x - are zeroes (we ignore them), 000..., 111... etc. - bits of corresponding digit in base64
+    //                                                ^^^^ - these 4 are also zeroes. So the first guid char can only be 0, 1, 2 or 3
+    // uint24 number in binary: xxxxxxxx_00000011_11112222_22333333
     let result = "";
     result += guidChars[uint24s[0] >> 6];
     result += guidChars[uint24s[0] & 63];
