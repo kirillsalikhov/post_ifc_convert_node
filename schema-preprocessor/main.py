7bc1184abb63c8ce86b92833b4ff3d2ab1551482
kirill <kirill.salikhov@gmail.com>
2024-02-05 01:14:41 +0400

    refactor(schema-generator) moved psets defs
    refs: IV-CS-SUPPORT-T-77

--- schema-preprocessor/main.py
+++ schema-preprocessor/main.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import json
 from pprint import pprint as pp
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "ifcopenshell"))
@@ -10,10 +9,8 @@ import ifcopenshell.util.pset
 import ifcopenshell.util.element
 from ifcopenshell import util
 
-from utils import erp_short_type
-from units import get_unit_type
-
 from entities_defs import serialize_entity_defs
+from psets_defs import serialize_psets
 
 # TODO move somehere
 # debug func
@@ -25,94 +22,6 @@ def uniq_pp(o_1, o_2):
         pp(o_1)
         pp(o_2)
 
-def serialize_psets(schema, out_path):
-    data = {}
-
-    psets = util.pset.PsetQto(schema.name()).get_applicable()
-
-    for pset in psets:
-        data[pset.Name.upper()] = pset_def(pset, schema)
-
-    if (schema.name() == "IFC2X3"):
-        for extra_pset in extra_ifc2x3_psets(schema):
-            data[extra_pset.get_name()] = extra_pset.as_json()
-
-    with open(out_path, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-
-
-def _prop_type_str(type_name, schema):
-    # strip because there was ' IfcPowerMeasure' with leading space
-    declaration = schema.declaration_by_name(type_name.strip())
-    return str(declaration)
-
-def pset_def(pset, schema):
-    def _unit_type(unit):
-        if (unit is None):
-            return None
-        if (not unit.is_a("IfcNamedUnit")):
-            return None
-        return unit.UnitType
-
-    # it's needed because there are mismatches in prop names in xml
-    def _propKey(prop):
-        return prop.Name.upper()
-
-    def _serialize_prop(prop):       
-        return {
-             **erp_short_type(_prop_type_str(prop.PrimaryMeasureType,  schema)), # merge
-            "unit": _unit_type(prop.PrimaryUnit)           
-        }
-
-    pset_d = {}
-    for prop in pset.HasPropertyTemplates:
-        if (prop.TemplateType == "P_COMPLEX"):
-            prop_d = {
-                "props": {}
-            }
-            for sub_prop in prop.HasPropertyTemplates:
-                prop_d["props"][_propKey(sub_prop)] = _serialize_prop(sub_prop)
-        else:
-            prop_d = _serialize_prop(prop)
-
-        pset_d[_propKey(prop)] = prop_d
-
-    return pset_d
-
-
-class ExtraPset():
-    def __init__(self, name, schema):
-        self.schema = schema
-        self.name = name
-        self.props_defs = {}
-
-    def add_prop(self, name, prop_type_name):
-        prop_type = self.schema.declaration_by_name(prop_type_name)
-        
-        self.props_defs[name.upper()] = {
-            **erp_short_type(str(prop_type)),
-            "unit": get_unit_type(prop_type)
-        }
-        return self
-
-    def get_name(self):
-        return self.name.upper()
-
-    def as_json(self):
-        return self.props_defs
-
-
-def extra_ifc2x3_psets(schema):
-    # https://standards.buildingsmart.org/documents/Implementation/IFC_Implementation_Agreements/CV-2x3-157.html
-    Pset_ProvisionForVoid = ExtraPset("Pset_ProvisionForVoid", schema) \
-        .add_prop("Width", "IfcLengthMeasure") \
-        .add_prop("Height", "IfcLengthMeasure") \
-        .add_prop("Diameter", "IfcLengthMeasure") \
-        .add_prop("Depth", "IfcLengthMeasure") \
-        .add_prop("Shape", "IfcLabel") \
-        .add_prop("System", "IfcLabel") \
-
-    return [Pset_ProvisionForVoid]
 
 GEN_PATH = "../src/convert-xml/schema/gen"
 
