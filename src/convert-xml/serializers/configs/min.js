3d4acc72d9a50b4555264313e37bd6d4dde02173
kirill <kirill.salikhov@gmail.com>
2024-02-11 14:33:58 +0400

    fix: add id to min serializer
    it's same as GlobalId but gltf rename use it
    I didn't remove GlobalId in case we are using it
    
    converter version bump 1.3.2
    
    refs: IV-CS-SUPPORT-T-91

--- src/convert-xml/serializers/configs/min.js
+++ src/convert-xml/serializers/configs/min.js
@@ -2,11 +2,14 @@ const {
     combineModifiers,
     internal,
     nodeChildren,
-    typeTitle
-    } = require("../accessors");
+    typeTitle,
+} = require("../accessors");
+
+const id = (el, res) => res["id"] = el.attributes.id;
 
 const node = [
     combineModifiers([
+        id,
         internal,
         nodeChildren,
     ]), typeTitle]
